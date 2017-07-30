53a9895d5eff03014e5bba76b1b98b9c8a87fd12
YouTakaoka <pinokions009@gmail.com>
2017-07-30 09:47:58 +0900

    updated java/MyInput/Main.java

---
+++ java/MyInput/Main.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main {
+    public void main(String args[]) {
+        MyInput<String,Future<Integer>> input = new MyInput<>();
+        ThreadCreator tc = new ThreadCreator<>();
+        List<Future<Integer>> threads = input.carryOut(tc::create);
+        
+        for(Future<Integer> ret: threads){
+            System.out.println(ret.get());
+        }
+    }
+    
+    class ThreadCreator {
+        String in;
+        ExecutorService executor;
+
+        public Future<Integer> create(String in) {
+            Future<Integer> ret = executor.submit(new Worker(in));
+            return ret;
+        };
+    }
+
+    class Worker implements Callable<Integer> {
+        String s;
+            
+        public Worker(String s){
+            this.s = s;
+        }
+            
+        @Override
+        public Integer call(){
+            int i;
+            for(i = 0 ; i < Integer.parseInt(s) ; i++){}
+            return i;
+        }
+    }
+}
+
+class MyInput<S,T> {
+    public List<T> carryOut(MyExec<S,T> method){
+        List<T> res;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String s = sc.nextLine();
+            res.add(method.exec(s));
+        }
+        return res;
+    }
+}
+
+@FunctionalInterface
+interface MyExec<S,T> {
+    T exec(S in);
+}
