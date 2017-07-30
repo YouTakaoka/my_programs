9b36db445b8507d6f2294314cb7879f69c98cd90
YouTakaoka <pinokions009@gmail.com>
2017-07-30 10:44:41 +0900

    updated java/MyInput/Main.java

--- java/MyInput/Main.java
+++ java/MyInput/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class Main {
-    public void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException, ExecutionException {
         MyInput<String,Future<Integer>> input = new MyInput<>();
         ThreadCreator tc = new ThreadCreator();
         List<Future<Integer>> threads = input.carryOut(tc::create);
@@ -11,39 +11,41 @@ public class Main {
             System.out.println(ret.get());
         }
     }
-    
-    class ThreadCreator {
-        ExecutorService executor;
-        
-        public void ThreadCreator(){
-            executor =  Executors.newCachedThreadPool();
-        }
+}
 
-        public Future<Integer> create(String in) {
-            Future<Integer> ret = executor.submit(new Worker(in));
-            return ret;
-        };
+    
+class ThreadCreator {
+    ExecutorService executor;
+        
+    public void ThreadCreator(){
+        executor =  Executors.newCachedThreadPool();
     }
 
-    class Worker implements Callable<Integer> {
-        String s;
+    public Future<Integer> create(String in) {
+        Future<Integer> ret = executor.submit(new Worker(in));
+        return ret;
+    };
+}
+
+class Worker implements Callable<Integer> {
+    String s;
             
-        public Worker(String s){
-            this.s = s;
-        }
+    public Worker(String s){
+        this.s = s;
+    }
             
-        @Override
-        public Integer call(){
-            int i;
-            for(i = 0 ; i < Integer.parseInt(s) ; i++){}
-            return i;
-        }
+    @Override
+    public Integer call(){
+        int i;
+        for(i = 0 ; i < Integer.parseInt(s) ; i++){}
+        return i;
     }
 }
 
+
 class MyInput<S,T> {
     public List<T> carryOut(MyExec<String,T> method){
-        List<T> res;
+        List<T> res = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
             String s = sc.nextLine();
