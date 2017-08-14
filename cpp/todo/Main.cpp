3e8b5a06ead0239adff4c23bfb275853e7e8c0dc
You Takaoka <pinokions009@gmail.com>
2017-08-14 15:40:12 +0900

    updated Main.cpp Main.hpp Makefile

--- cpp/todo/Main.cpp
+++ cpp/todo/Main.cpp
@@ -2,16 +2,56 @@
 #include<Main.hpp>
 using namespace std;
 
-Task::Task(string name, string content = "") {
+Searchable::Searchable(string name) {
   this->name = name;
-  this->content = content;
 }
 
-Frag::Frag(string name) {
-  this->name = name;
+string Searchable::getName() {
+  return this->name;
+}
+
+Flag::Flag(string name, string explanation = "") : Searchable(name) {
+  if(Flag::exists(name)) {
+    throw FlagExistsException(name);
+  }
+  this->explanation = explanation;
+  Flag::nameToObj[name] = this;  
+}
+
+bool Flag::exists(string name) {
+  if(Flag::nameToObj.count(name) > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Task::Task(string name, string content = "") : Searchable(name) {
+  this->content = content;
+  Task::nameToObj[name] = this;  
+}
+
+bool Task::hasFlag(Flag flag) {
+  if(this->checkedFlags.count(flag.name) > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void Task::addFlag(Flag flag) {
+  if(Task::hasFlag(flag)){
+    throw HasFlagException(this, flag)
+  }
+  checkedFlags[flag.name] = flag;
+}
+
+void removeFlag(Flag flag) {
+  if(!this->hasFlag()){
+    throw NoFlagException(this, flag);
+  }
 }
 
 int main() {
-
-  
+  //  
 }
--- cpp/todo/Main.hpp
+++ cpp/todo/Main.hpp
@@ -1,19 +1,79 @@
 #include<iostream>
 #include<list>
+#include<map>
 using namespace std;
 
-class Task {
+class Searchable {
+  /**
+   * Class that contains map structure so it can be searched by name.
+   */
 private:
   string name;
-  string content;
-  list<::Frag> checkedFrags;
 public:
-  Task(string name, string content = "");
+  string getName();
 }
 
-class Frag {
+class Flag : public Searchable {
 private:
-  string name;
+  string explanation;
+  static map<string, Flag*> nameToObj;
 public:
-  Frag(string name);
+  Flag(string name, string explanation = "");
+  statc bool exists(string name);
+}
+
+class FlagExistsException : public exception {
+private:
+  string mes;
+  string name; // flag name which has been attempted to create.
+public:
+  FlagExistsException(string name){
+    this->name = name;
+    this->mes = "Flag "+ name +" already exists.";
+  }
+  string getMes(){
+    return this->mes;
+  }
+}
+
+class Task : public Searchable {
+private:
+  string content;
+  map<string, Flag> checkedFlags;
+  static map<string, Task*> nameToObj;
+public:
+  Task(string name, string content = "");
+  bool hasFlag(Flag flag);
+  void addFlag(Flag flag);
+  void removeFlag(Flag flag);
+}
+
+class TaskFlagException : public exception {
+protected:
+  string mes;
+  string taskName;
+  string flagName;
+public:
+  TaskFlagException(Task task, Flag flag) {
+    this->taskName = task.getName();
+    this->flagName = flag.getName();
+  }
+  string getMes() {
+    return this->mes;
+  }
+
+}
+
+class HasFlagException : public TaskFlagException {
+public:
+  HasFlagException(Task task, Flag flag) : TaskFlagException(task, flag) {
+    this->mes = "Task "+ taskName + " already has flag "+ this->flagName;
+  }
+}
+
+class NoFlagException : public TaskFlagException {
+public:
+  NoFlagException(Task task, Flag flag) : TaskFlagException(task, flag){
+    this->mes = "Task "+ taskName + " does not have flag "+ flagName;
+  }
 }
